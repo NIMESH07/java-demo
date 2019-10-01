9d317a2b365e3b95bfe3091480bcda8be92e8749
Nimesh <Nimesh@DESKTOP-5GACCUN>
2019-10-01 18:52:15 +0530

    JavaServlet

---
+++ src/changeam/ChangeAM.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package changeam;
+
+import sun.management.MethodInfo;
+
+
+
+/**
+ *
+ * @author Nimesh
+ */
+public class ChangeAM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Abc a1= new Abc();
+        /*MyClass instance = new MyClass();
+MethodInfo yourMethod = instance
+                            .GetType()
+                            .GetMethod("MyMethod", BindingFlags.NonPublic | BindingFlags.Instance);
+var returnValue = yourMethod.Invoke(instance, new object[] { });
+Console.WriteLine(returnValue);*/
+       // a1.show();
+       
+    }
+    public void n1()
+    {
+        
+    }
+    
+    
+    
+    
+}
+class Abc
+{
+    private void show()
+    {
+        System.out.println("Hello");
+    }
+}
